fc3309cb257a2470714d7ac9f9530b63f7d6d3e5
Drakkarr <72389960+Drakkarr@users.noreply.github.com>
2020-10-14 10:24:31 +0200

    Fonction reglesDuJeu complete

--- src/com/company/FonctionsMenu.java
+++ src/com/company/FonctionsMenu.java
@@ -2,14 +2,17 @@ package com.company;
 
 public class FonctionsMenu {
     public static void reglesDuJeu() {
-        System.out.println("--Vous pouvez vous déplacer horizontalement ou verticalement--");
-        System.out.println("Pour vous déplacer dans la direction :");
+        System.out.println("//-REGLES DU JEU-//");
+        System.out.println("--A chaque tour vous pouvez vous déplacer horizontalement ou verticalement--");
+        System.out.println("Appuyez sur les touches suivantes pour vous déplacer dans la direction :");
         System.out.println("/Droite --> D");
         System.out.println("/Gauche --> G");
         System.out.println("/Haut --> Z");
         System.out.println("/Bas --> S");
         System.out.println();
-        System.out.println("--A chaque ");
+        System.out.println("--A chaque tour les joueurs doit séléctionner une case à détruire--");
+        System.out.println("--Le but du jeu est de coincé l'adversaire, dans ce cas :");
+        System.out.println("C'EST GAGNE--");
 
     }
 }
