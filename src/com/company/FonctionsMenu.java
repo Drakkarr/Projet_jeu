4624635290e73e3786e6df9307e2c5ae70ad7843
Mélyne LERAY <mon_mailmelyne.leray@edu.itescia.fr>
2020-10-14 09:16:55 +0200

    Ajout de la FonctionDemarrage

--- src/com/company/FonctionsMenu.java
+++ src/com/company/FonctionsMenu.java
@@ -1,6 +1,18 @@
 package com.company;
 
 public class FonctionsMenu {
+
+    public class FonctionDemarrage {
+
+        public static void FonctionDemarrage(){
+
+            /**
+             * Affichage de la fonction démarrage
+             */
+
+            System.out.println("Bonjour et Bienvenue !" );
+        }
+    }
     public static void reglesDuJeu() {
         System.out.println("--Vous pouvez vous déplacer horizontalement ou verticalement--");
         System.out.println("Pour vous déplacer dans la direction :");
