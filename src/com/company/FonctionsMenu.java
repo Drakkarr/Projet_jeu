caf94a24a876d9bf0a11a91be387b27343d170a4
Drakkarr <72389960+Drakkarr@users.noreply.github.com>
2020-10-14 09:10:29 +0200

    Ajout de la classe FonctionsMenu + reglesDuJeu

---
+++ src/com/company/FonctionsMenu.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class FonctionsMenu {
+    public static void reglesDuJeu() {
+        System.out.println("--Vous pouvez vous déplacer horizontalement ou verticalement--");
+        System.out.println("Pour vous déplacer dans la direction :");
+        System.out.println("/Droite --> D");
+        System.out.println("/Gauche --> G");
+        System.out.println("/Haut --> Z");
+        System.out.println("/Bas --> S");
+        System.out.println();
+        System.out.println("--A chaque ");
+
+    }
+}
