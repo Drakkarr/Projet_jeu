3981ae6dfa4ecd548bc3b27901c682c9151629bf
Drakkarr <72389960+Drakkarr@users.noreply.github.com>
2020-10-14 15:43:19 +0200

    ajout de la fonctionnalité de destruction + correction fautes de frappe

--- src/com/company/FonctionsMenu.java
+++ src/com/company/FonctionsMenu.java
@@ -15,18 +15,23 @@ public class FonctionsMenu {
             System.out.println("Tape 4 pour quitter la partie");
         }
 
-    public static void reglesDuJeu() {
-        System.out.println("//-REGLES DU JEU-//");
-        System.out.println("--A chaque tour vous pouvez vous déplacer horizontalement ou verticalement--");
-        System.out.println("Appuyez sur les touches suivantes pour vous déplacer dans la direction :");
-        System.out.println("/Droite --> D");
-        System.out.println("/Gauche --> G");
-        System.out.println("/Haut --> Z");
-        System.out.println("/Bas --> S");
-        System.out.println();
-        System.out.println("--A chaque tour les joueurs doit séléctionner une case à détruire--");
-        System.out.println("--Le but du jeu est de coincé l'adversaire, dans ce cas :");
-        System.out.println("C'EST GAGNE--");
+        public static void reglesDuJeu() {
 
-    }
+            /**
+             * Affichage des règles du jeu
+             */
+
+            System.out.println("//-REGLES DU JEU-//");
+            System.out.println("--A chaque tour vous pouvez vous déplacer horizontalement ou verticalement--");
+            System.out.println("Appuyez sur les touches suivantes pour vous déplacer dans la direction :");
+            System.out.println("/Droite --> D");
+            System.out.println("/Gauche --> Q");
+            System.out.println("/Haut --> Z");
+            System.out.println("/Bas --> S");
+            System.out.println();
+            System.out.println("--A chaque tour les joueurs doivent séléctionner une case à détruire--");
+            System.out.println("--Le but du jeu est de coincé l'adversaire, dans ce cas :");
+            System.out.println("C'EST GAGNE--");
+
+        }
 }
--- src/com/company/PlateauDeJeu.java
+++ src/com/company/PlateauDeJeu.java
@@ -4,24 +4,39 @@ import java.util.Scanner;
 
 public class PlateauDeJeu {
 
-    public static void affichagePlateau(){
+
+    static int inputValueX() {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        return input;
+    }
+
+    static int inputValueY() {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        return input;
+    }
+
+    public static void affichagePlateau() {
         String p1 = "B"; // Joueur1
         String p2 = "R"; // Joueur2
         String currentPlayer = p1; // Joueur actuelle est p1
 
-        String [][] matrice = new String[10][11];
+        String[][] matrice = new String[10][11];
         int ligne;
         int colonne;
 
-        while(true){
+        while (true) {
             //remplir toutes les cases
-            for (ligne=0; ligne<=9; ligne++){
-                for (colonne =0; colonne<=10; colonne++){
+            for (ligne = 0; ligne <= 9; ligne++) {
+                for (colonne = 0; colonne <= 10; colonne++) {
                     matrice[ligne][colonne] = "*";
-                    if (ligne ==4 && colonne==5){
+                    //placer le joueur 1
+                    if (ligne == 4 && colonne == 5) {
                         matrice[ligne][colonne] = p1;
                     }
-                    if (ligne ==5 && colonne==5){
+                    //placer le joueur 2
+                    if (ligne == 5 && colonne == 5) {
                         matrice[ligne][colonne] = p2;
                     }
 
@@ -29,8 +44,8 @@ public class PlateauDeJeu {
             }
 
             //afficher toutes les cases
-            for (ligne=0; ligne<=9; ligne++) {
-                for (colonne = 0; colonne <=10; colonne++) {
+            for (ligne = 0; ligne <= 9; ligne++) {
+                for (colonne = 0; colonne <= 10; colonne++) {
                     System.out.print(" | " + matrice[ligne][colonne]);
 
                 }
@@ -38,17 +53,35 @@ public class PlateauDeJeu {
                 System.out.println(" | ");
             }
             System.out.println(currentPlayer + " a toi de jouer");
-            Scanner scanner1 = new Scanner(System.in);
-            String recupScanner = scanner1.next();
-            // si joueur actuel est Joueur 1 il devient joueur 2
-            if(currentPlayer == p1){
-                currentPlayer = p2;
-            }
-            else{ // Si joueur 2 il devient joueur 1
-                currentPlayer = p1;
-            }
-        }
+
+
+
+                int choixP1ligne = inputValueX();
+                int choixP1colonne = inputValueY();
+
+
+                    matrice[choixP1colonne][choixP1ligne]= "X";
+
+                    //afficher les cases
+                    for (ligne = 0; ligne <= 9; ligne++) {
+                        for (colonne = 0; colonne <= 10; colonne++) {
+                            System.out.print(" | " + matrice[ligne][colonne]);
+
+                        }
+                        //saute une ligne
+                        System.out.println(" | ");
+                    }
+
+                        // si joueur actuel est Joueur 1 il devient joueur 2
+                        if (currentPlayer == p1) {
+                            currentPlayer = p2;
+                        } else { // Si joueur 2 il devient joueur 1
+                            currentPlayer = p1;
+                        }
+
+
+
 
         }
-
+    }
 }
