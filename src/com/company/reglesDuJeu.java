72bb7979a013fa08e036b06df2ceae2e8cb13ec6
Mélyne LERAY <mon_mailmelyne.leray@edu.itescia.fr>
2020-10-15 16:02:50 +0200

    Création class reglesDuJeu et ajout de JavaDoc

---
+++ src/com/company/reglesDuJeu.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class reglesDuJeu {
+
+    public static void reglesDuJeu() {
+
+        /**
+         * Cette fonction reglesDuJeu est caractérisée par les informations suivantes:
+         * @see reglesDuJeu
+         * La fonction reglesDuJeu permet d'avoir un rappelle des règles du jeu :
+         * REGLES DU JEU :
+         * A chaque tour vous pouvez vous déplacer horizontalement ou verticalement.
+         * Appuyez sur les touches suivantes pour vous déplacer dans la direction.
+         * Droite --> D.
+         * Gauche --> Q.
+         * Haut --> Z.
+         * Bas --> S.
+         * A chaque tour les joueurs doivent séléctionner une case à détruire.
+         * Le but du jeu est de coincé l'adversaire, dans ce cas :
+         * C'EST GAGNE.
+         */
+
+        // Affichage de la fonction reglesDuJeu
+
+        System.out.println("//-REGLES DU JEU-//");
+        System.out.println("--A chaque tour vous pouvez vous déplacer horizontalement ou verticalement--");
+        System.out.println("Appuyez sur les touches suivantes pour vous déplacer dans la direction :");
+        System.out.println("/Droite --> D");
+        System.out.println("/Gauche --> Q");
+        System.out.println("/Haut --> Z");
+        System.out.println("/Bas --> S");
+        System.out.println();
+        System.out.println("--A chaque tour les joueurs doivent séléctionner une case à détruire--");
+        System.out.println("--Le but du jeu est de coincé l'adversaire, dans ce cas :");
+        System.out.println("C'EST GAGNE--");
+        System.out.println();
+    }
+}
