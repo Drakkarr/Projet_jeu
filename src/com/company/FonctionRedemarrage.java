953c687bbf7d6ecc80a508dcef838a86ba26df2f
Mélyne LERAY <mon_mailmelyne.leray@edu.itescia.fr>
2020-10-15 16:03:24 +0200

    Création class FonctionRedemarrage et ajout de JavaDoc

---
+++ src/com/company/FonctionRedemarrage.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class FonctionRedemarrage {
+
+    public static void FonctionRedemarrage() {
+
+        /**
+         * Cette fonction FonctionRedemarrage est caractérisée par les informations suivantes:
+         * @see FonctionRedemarrage
+         * FonctionRedemarrage est la fonction qui permet de rappeler le menu du jeu.
+         * Le menu est composé de la fonction FonctionDemarrage, FonctionRedemarrage et de la fonction reglesDuJeu.
+         * La fonction FonctionDemarrage permet d'être redirigé une nouvelle fois là où on le souhaite :
+         * 1 pour voir les règles, 2 pour démarrer la partie, 3 pour voir le score, 4 pour quitter la partie.
+         */
+
+        // Affichage de la fonction FonctionRedemarrage
+
+        System.out.println("Tape 1 pour voir les règles");
+        System.out.println("Tape 2 pour démarrer une partie");
+        System.out.println("Tape 3 pour voir le score");
+        System.out.println("Tape 4 pour quitter la partie");
+    }
+}
