feb18a417ac753606761eb533be2ee973ab9e813
Drakkarr <72389960+Drakkarr@users.noreply.github.com>
2020-10-15 09:11:15 +0200

    Story 2, revenir au menu après affichage règles

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import java.io.Console;
-
 import java.util.Scanner;
 
 public class Main {
@@ -18,6 +16,26 @@ public class Main {
         // Si l'utilisateur saisi "1" lance fonction règle du jeu
         if (recupScannerMenu.equals("1")) {
             FonctionsMenu.reglesDuJeu();
+            FonctionsMenu.FonctionDemarrage();
+
+            if (recupScannerMenu.equals("1")) {
+                FonctionsMenu.reglesDuJeu();
+            }
+            // Si l'utilisateur saisi "2" lance fonction jeu
+            else if (recupScannerMenu.equals("2")) {
+                PlateauDeJeu.affichagePlateau();
+            }
+            // Si l'utilisateur saisi "3" lance l'historique des parties
+            else if (recupScannerMenu.equals("3")) {
+                System.out.println("Historique parti");
+            }
+            // Si l'utilisateur saisi "4" quitte le jeu
+            else if (recupScannerMenu.equals("4")) {
+            }
+            // Si l'utilisateur saisi une autre commande que celles proposées message + relance le menu
+            else {
+                System.out.println("Commande non valide");
+            }
         }
         // Si l'utilisateur saisi "2" lance fonction jeu
         else if (recupScannerMenu.equals("2")) {
